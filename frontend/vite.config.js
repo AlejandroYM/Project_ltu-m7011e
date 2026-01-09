3a445bf64ad370c8b78945769346575fe4aa26f2
alejandro_ym3 <aleyec-5@student.ltu.se>
2026-01-09 23:49:56 +0100

    Corrigiendo rutas, puertos y formato de datos

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api/users': 'http://localhost:3001',
-      '/api/recipes': 'http://localhost:3002',
-      '/api/recommendations': 'http://localhost:3003',
-    }
+    port: 5173, // Puerto estándar de Vite
   }
 })
